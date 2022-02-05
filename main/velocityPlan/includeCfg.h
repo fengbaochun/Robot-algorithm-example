099629bd5748244316bfcb9d8cd58a00189b3e71
fengbaochun <849067797@qq.com>
2022-02-05 17:44:36 +0800

    kinematicsDemo test

--- main/velocityPlan/includeCfg.h
+++ main/velocityPlan/includeCfg.h
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 //使用串口发送
 //#define USE_SERIAL_SEND	
 
+
+/*
+参考：
+https://openrsl.blog.csdn.net/article/details/95792841
+
+*/
--- main/velocityPlan/main.cpp
+++ main/velocityPlan/main.cpp
@@ -1,13 +1,15 @@
 ﻿#include <stdio.h>
 #include "velocityPlan.h"
+#include "kinematics.h"
 
 int main(void)
 {
 #ifdef USE_SERIAL_SEND
 	serial_test();
 #endif // USE_SERIAL_SEND	
-	velocityPlanInit();
-	
+	//velocityPlanInit();
+
+	kinematicsDemo();
 	while (1) {
 	}
 	return 0;
