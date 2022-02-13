d5ed537589cd2dae8490eccc4486d29375d64dd1
fengbaochun <849067797@qq.com>
2022-02-13 21:22:49 +0800

    demo 测试

--- alg/Tspeed.m
+++ alg/Tspeed.m
@@ -1,38 +1,40 @@
-%% 主函数 名字必须与文件一致
-function Tspeed()
-    %% input para 
-    %% q0和v0分别是起始点的位置和速度, 
-    %% q1和v1分别是终点的位置和速度。
-    %% vmax, amax分别是是规划的最大速度和最大加速度。
-    clear;
-    q0 = 100;     v0 = 50; 
-    q1 = 300;   v1 =50;
-    vmax = 150; 
-    amax = 100;
-    ad=-200;
+% %% 主函数 名字必须与文件一致
+% function Tspeed()
+%     %% input para 
+%     %% q0和v0分别是起始点的位置和速度, 
+%     %% q1和v1分别是终点的位置和速度。
+%     %% vmax, amax分别是是规划的最大速度和最大加速度。
+%     clear;
+%     q0 = 100;     v0 = 50; 
+%     q1 = 300;   v1 =50;
+%     vmax = 150; 
+%     amax = 100;
+%     dmax=-200;
+% 
+%     [time, q, qd, qdd] = TSpeedCruveMethod(q0,q1,v0,v1,vmax,amax,dmax);
+% 
+%     figure(1)
+%     subplot(311)
+%     plot(time,q,'r','LineWidth',1.5);
+%     grid on;xlabel('time[s]');ylabel('position[mm]');
+% 
+%     subplot(312)
+%     plot(time,qd,'b','LineWidth',1.5);
+%     grid on;xlabel('time[s]');ylabel('speed[mm/s]');
+% 
+%     subplot(313)
+%     plot(time,qdd,'g','LineWidth',1.5);
+%     grid on;xlabel('time[s]');ylabel('acceleration[mm/s2]');
+% end
 
-    [time, q, qd, qdd] = TSpeedCruveMethod(q0,q1,v0,v1,vmax,amax,ad);
+% function [time, q, qd, qdd] = TSpeedCruveMethod(q0,q1,v0,v1,vmax,amax,dmax)
 
-    figure(1)
-    subplot(311)
-    plot(time,q,'r','LineWidth',1.5);
-    grid on;xlabel('time[s]');ylabel('position[mm]');
-
-    subplot(312)
-    plot(time,qd,'b','LineWidth',1.5);
-    grid on;xlabel('time[s]');ylabel('speed[mm/s]');
-
-    subplot(313)
-    plot(time,qdd,'g','LineWidth',1.5);
-    grid on;xlabel('time[s]');ylabel('acceleration[mm/s2]');
-end
-
-function [time, q, qd, qdd] = TSpeedCruveMethod(q0,q1,v0,v1,vmax,amax,ad)
+function [time, q, qd, qdd] = Tspeed(q0,q1,v0,v1,vmax,amax,dmax)
     %% input q0,q1,vo,v1,vmax,amax
     %% output time,q,qd,qdd
     h = q1-q0;
     % 可达到的最大速度
-    v_temp = sqrt((2.0*amax*ad*h - amax*v1^2 + ad*v0^2) / (ad-amax));
+    v_temp = sqrt((2.0*amax*dmax*h - amax*v1^2 + dmax*v0^2) / (dmax-amax));
 
     % 确定匀速阶段速度
     if(v_temp<vmax)
@@ -46,12 +48,12 @@ function [time, q, qd, qdd] = TSpeedCruveMethod(q0,q1,v0,v1,vmax,amax,ad)
     Sa = v0*Ta+(1.0/2.0)*amax*Ta^2;
 
     % 计算匀速阶段的时间和位移
-    Tv = (h-(vlim^2-v0^2)/(2*amax)-(v1^2-vlim^2)/(2*ad))/vlim;
+    Tv = (h-(vlim^2-v0^2)/(2*amax)-(v1^2-vlim^2)/(2*dmax))/vlim;
     Sv = vlim*Tv;
 
     % 计算减速阶段的时间和位移
-    Td = (v1-vlim)/ad;
-    Sd = vlim*Td + (1.0/2.0)*ad*Td^2;
+    Td = (v1-vlim)/dmax;
+    Sd = vlim*Td + (1.0/2.0)*dmax*Td^2;
 
     T = Ta + Tv +Td;
     disp("----------------------------------------------------")
@@ -78,9 +80,9 @@ function [time, q, qd, qdd] = TSpeedCruveMethod(q0,q1,v0,v1,vmax,amax,ad)
             qd(k) = vlim;
             qdd(k) = 0;
         elseif(t >= Ta+Tv && t <= T)
-            q(k) = q0 + Sa + Sv + vlim*(t - Ta - Tv) + (1.0/2.0)*ad*power(t - Ta - Tv, 2);
-            qd(k) = vlim + ad *(t - Ta - Tv);
-            qdd(k) = ad;
+            q(k) = q0 + Sa + Sv + vlim*(t - Ta - Tv) + (1.0/2.0)*dmax*power(t - Ta - Tv, 2);
+            qd(k) = vlim + dmax *(t - Ta - Tv);
+            qdd(k) = dmax;
         end
         disp("k = " + k + ", t = " + time(k)+ ", q = " + q(k)+ ", qd = " + qd(k)+", qdd = " + qdd(k))
         k = k + 1;
--- alg/main.m
+++ alg/main.m
@@ -9,13 +9,13 @@ function main()
 	% 插补参数
     startPoint = [-220,0,-140];  %插补起点
     endPoint    = [220,-20,140];       %插补终点
-    speed = 40;                     %插补速度
+    speed = 80;                     %插补速度
     t0 = startT;                    %初始位姿
     [x,y,z,T] = lineInterpolation(t0,startPoint,endPoint,speed);    %插补结果
     % 显示轨迹
     Tj=transl(T);                   %将平移部分提取出来
     plot3(Tj(:,1),Tj(:,2),Tj(:,3),'-o','Color','b','MarkerSize',3,'MarkerFaceColor','#D9FFFF');%输出末端轨迹
-    
+%     plot3(x,y,z,'r'),xlabel('x'),ylabel('y'),zlabel('z'),hold on,plot3(x,y,z,'o','color','g'),grid on;
     %循环显示
     for i=1:1:50
         q=robot.ikine(T);           %逆解输出位姿p，求解出关节角q
